f6da293c4c39d81fc59c74e2af255d44dc8b4eff
Colyn <colyn.brown@gmail.com>
2020-02-09 16:57:21 -0700

    fixed og iamge

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ export default function(Vue, { router, head, isClient }) {
   })
 
   head.meta.push({
-    name: 'og:image',
+    property: 'og:image',
     content: '/assets/static/src/favicon.png'
   })
 }
